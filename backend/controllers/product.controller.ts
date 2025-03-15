1fb0338b253284449f8c7328b7bfe15c75968315
Shenelle Peart <speart@nabrlee.com>
2025-03-15 16:14:35 -0500

    Updated status errors

--- backend/controllers/product.controller.ts
+++ backend/controllers/product.controller.ts
@@ -52,11 +52,15 @@ export const updateProduct = async (req: Request, res: Response) => {
 
 export const deleteProduct = async (req: Request, res: Response) => {
   const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ success: false, message: "Invalid product id" });
+  }
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product Deleted" });
   } catch (error) {
     console.log("error in deleting product:", (error as Error).message);
-    res.status(404).json({ success: false, message: "Product not found" });
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 };
