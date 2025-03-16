ddc481cc8264ae350c07fdda4a0036c49d0ff33d
Shenelle Peart <speart@nabrlee.com>
2025-03-15 23:17:48 -0500

    added other methods to zustand store

--- frontend/src/store/product.ts
+++ frontend/src/store/product.ts
@@ -1,16 +1,13 @@
 import { create } from "zustand";
+import { Product } from "../types";
 
-type Product = {
-  name: string;
-  price: string;
-  image: string;
-};
 type ProductState = {
   products: Product[];
   setProducts: (products: Product[]) => void;
   createProduct: (
     newProduct: Product
   ) => Promise<{ success: boolean; message: string }>;
+  fetchProducts: () => void;
 };
 
 export const useProductStore = create<ProductState>((set) => ({
@@ -31,4 +28,16 @@ export const useProductStore = create<ProductState>((set) => ({
     set((state) => ({ products: [...state.products, data.data] }));
     return { success: true, message: "Product created successfully" };
   },
+  fetchProducts: async () => {
+    const res = await fetch("/api/products", {});
+    const data = await res.json();
+    set({ products: data.data });
+  },
+  deleteProduct: async (id: string) => {
+    const res = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+  },
+  updateProduct: async (id: string) => {},
 }));
